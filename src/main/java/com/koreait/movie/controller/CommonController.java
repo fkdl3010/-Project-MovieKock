e80a5d76bc3c58a986a7732d957d65e97b4be7b6
fkdl3010 <fkdl3919@icloud.com>
2021-02-02 16:36:39 +0900

    no message

--- src/main/java/com/koreait/movie/controller/CommonController.java
+++ src/main/java/com/koreait/movie/controller/CommonController.java
@@ -1,22 +1,15 @@
 package com.koreait.movie.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
-	
-	
+
+
 	@RequestMapping(value="")
 	public String index() {
 		return "index";
 	}
-	
-	
-	
-	
-	
-	
 	
 }
