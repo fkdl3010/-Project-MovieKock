2ce54e14ab60bd1812fc6864744f009570f592c4
CHOIHEARI <wlazhd00@naver.com>
2021-02-02 15:47:59 +0900

    userDto 완성

--- src/main/java/com/koreait/movie/dto/UserDto.java
+++ src/main/java/com/koreait/movie/dto/UserDto.java
@@ -1,7 +1,22 @@
 package com.koreait.movie.dto;
 
-public class UserDto {
-	
-	
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto {
+	private int user_no;
+	private String user_id;
+	private String user_pw;
+	private String user_nickname;
+	private String user_name;
+	private String user_email;
+	private String user_phone;
+	private Date user_date;
+	private String user_image_name;
 }
