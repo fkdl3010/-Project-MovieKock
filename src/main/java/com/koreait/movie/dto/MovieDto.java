6866c370345256c2caebf30c593711e66c54df54
fkdl3010 <fkdl3919@icloud.com>
2021-02-02 15:49:21 +0900

    Dto 생성

---
+++ src/main/java/com/koreait/movie/dto/MovieDto.java
@@ -0,0 +1,21 @@
+package com.koreait.movie.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MovieDto {
+	
+	private int movie_no;
+	private String movie_title;
+	private String movie_director;
+	private String movie_audience;
+	private Date movie_opening_date;
+	private String movie_nation;
+	
+}
