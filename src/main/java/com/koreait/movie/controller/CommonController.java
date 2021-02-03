1e2d1a138a1325d5dea588f4c0d8629ff20d2ffb
SPARK <realzidane05@naver.com>
2021-02-03 22:41:50 +0900

    02/03 22:41

--- src/main/java/com/koreait/movie/controller/CommonController.java
+++ src/main/java/com/koreait/movie/controller/CommonController.java
@@ -1,4 +1,4 @@
-package com.koreait.movie.controller;
+ package com.koreait.movie.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
