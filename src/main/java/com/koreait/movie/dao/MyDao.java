7a2957596e2378573a4f98baab65f5bb6f3c98eb
fkdl3010 <fkdl3919@icloud.com>
2021-02-05 04:56:20 +0900

    마이페이지 수정

--- src/main/java/com/koreait/movie/dao/MyDao.java
+++ src/main/java/com/koreait/movie/dao/MyDao.java
@@ -1,5 +1,10 @@
-package com.koreait.movie.dao;
+package com.koreait.movie.dao;import java.util.Map;
+
+import com.koreait.movie.dto.UserDto;
 
 public interface MyDao {
 
+	public int userUpdate(UserDto userDto);
+	
+	public UserDto sessionUser(int userNo);
 }
